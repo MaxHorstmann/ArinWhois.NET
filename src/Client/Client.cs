7e507a3b896b46dbda9833da2cd7f92cbfae85b0
Max Horstmann <mhorstmann@stackoverflow.com>
2014-06-18 10:41:19 -0400

    initial checkin

---
+++ src/Client/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace ArinWhois.Client
+{
+    public class Client
+    {
+        private const string BaseUrl = "http://whois.arin.net/rest";
+
+        private static string GetRequestUrl(string query)
+        {
+            return string.Format("{0}/{1}.json", BaseUrl, query);
+        }
+        
+
+
+    }
+
+}
---
+++ src/Model/Response.cs
@@ -0,0 +1,27 @@
+﻿namespace ArinWhois.Model
+{
+    public class Response
+    {
+        public Net net { get; set; }
+        public Org org { get; set; }
+        public Poc poc { get; set; }
+    }
+
+    public class Net
+    {
+        public object registrationDate { get; set; }
+    }
+
+    public class Org
+    {
+
+    }
+
+    public class Poc
+    {
+
+    }
+
+
+
+}
