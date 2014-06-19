ac41301a08a702f8739e74977debebfd7c800fc9
Max Horstmann <mhorstmann@stackoverflow.com>
2014-06-19 11:32:22 -0400

    update test

--- src/ArinWhois.Tests/ApiTests.cs
+++ src/ArinWhois.Tests/ApiTests.cs
@@ -12,9 +12,6 @@ namespace ArinWhois.Tests
         public void TestNetwork()
         {
             var arinClient = new ArinClient();
-
-            // http://whois.arin.net/rest/net/NET-69-63-176-0-1/pft.json
-            // sync over async, ok for test. http://blogs.msdn.com/b/pfxteam/archive/2012/04/13/10293638.aspx
             var response = arinClient.QueryAsync(IPAddress.Parse("69.63.176.0")).Result;
 
             Assert.IsNotNull(response.Network);
@@ -26,8 +23,6 @@ namespace ArinWhois.Tests
             Assert.IsNotNull(response.Network.NetBlocks.NetBlock);
             Assert.IsNotNull(response.Network.NetBlocks.NetBlock.CidrLength.Value);
             Assert.IsNotNull(response.Network.NetBlocks.NetBlock.Description);
-
-
         }
     }
 }
