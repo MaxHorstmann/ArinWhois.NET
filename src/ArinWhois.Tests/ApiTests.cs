6e4d9dd42d2681f6b02f36f0ce157a6b51d7e550
Max Horstmann <mhorstmann@stackoverflow.com>
2014-06-24 09:43:36 -0400

    refactor, organization

--- src/ArinWhois.Tests/ApiTests.cs
+++ src/ArinWhois.Tests/ApiTests.cs
@@ -12,20 +12,25 @@ namespace ArinWhois.Tests
         public void TestIpFound()
         {
             var arinClient = new ArinClient();
-            var response = arinClient.QueryIpAsync(IPAddress.Parse("69.63.176.0")).Result;
+            var ipResponse = arinClient.QueryIpAsync(IPAddress.Parse("69.63.176.0")).Result;
 
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Network);
+            Assert.IsNotNull(ipResponse);
+            Assert.IsNotNull(ipResponse.Network);
 
-            Assert.IsTrue(response.Network.TermsOfUse.StartsWith("http"));
-            Assert.IsNotNull(response.Network.RegistrationDate.Value);
-            Assert.IsNotNull(response.Network.NetBlocks.NetBlock);
-            Assert.IsNotNull(response.Network.NetBlocks.NetBlock.CidrLength.Value);
-            Assert.IsNotNull(response.Network.NetBlocks.NetBlock.Description);
+            Assert.IsTrue(ipResponse.Network.TermsOfUse.StartsWith("http"));
+            Assert.IsNotNull(ipResponse.Network.RegistrationDate.Value);
+            Assert.IsNotNull(ipResponse.Network.NetBlocks.NetBlock);
+            Assert.IsNotNull(ipResponse.Network.NetBlocks.NetBlock.CidrLength.Value);
+            Assert.IsNotNull(ipResponse.Network.NetBlocks.NetBlock.Description);
 
-            Assert.IsNotNull(response.Network.OrgRef.Name);
-            Assert.AreEqual(response.Network.OrgRef.Name, "Facebook, Inc.");
+            Assert.IsNotNull(ipResponse.Network.OrgRef.Name);
+            Assert.AreEqual(ipResponse.Network.OrgRef.Name, "Facebook, Inc.");
 
+            var organizationHandle = ipResponse.Network.OrgRef.Handle;
+            var organizationResponse = arinClient.QueryResourceAsync(organizationHandle, ArinClient.ResourceType.Organization).Result;
+
+            Assert.IsNotNull(organizationResponse);
+            Assert.AreEqual(organizationResponse.Organization.City.Value, "Menlo Park");
 
         }
 
@@ -35,21 +40,6 @@ namespace ArinWhois.Tests
             var arinClient = new ArinClient();
             var response = arinClient.QueryIpAsync(IPAddress.Parse("108.33.73.20")).Result;
             Assert.IsNull(response);
-        }
-
-
-        [TestMethod]
-        public void TestNetwork()
-        {
-            var arinClient = new ArinClient();
-            var response = arinClient.QueryNetworkAsync("NET-69-63-176-0-1").Result;
-
-            Assert.IsNotNull(response);
-            Assert.IsNotNull(response.Network);
-            Assert.IsNotNull(response.Organization);
-            Assert.IsNotNull(response.PointOfContact);
-
-            Assert.AreEqual(response.Organization.Name.Value, "Facebook, Inc.");
         }
 
 
--- src/Client/ArinClient.cs
+++ src/Client/ArinClient.cs
@@ -12,6 +12,14 @@ namespace ArinWhois.Client
 
         private static readonly Options DeserializationOptions = Options.ISO8601;
 
+        public enum ResourceType
+        {
+            Unknown = 0,
+            Network = 1,
+            Organization = 2,
+            PointOfContact = 3
+        }
+
         public async Task<Response> QueryIpAsync(IPAddress ip)
         {
             using (var wc = new WebClient())
@@ -31,13 +39,15 @@ namespace ArinWhois.Client
             }
         }
 
-        public async Task<Response> QueryNetworkAsync(string handle)
+        public async Task<Response> QueryResourceAsync(string handle, ResourceType resourceType)
         {
+            if (resourceType != ResourceType.Organization) throw new NotImplementedException(); // coming soon
+
             using (var wc = new WebClient())
             {
                 try
                 {
-                    var query = string.Format("net/{0}/pft", handle);
+                    var query = string.Format("org/{0}/pft", handle);
                     var jsonString = await wc.DownloadStringTaskAsync(GetRequestUrl(query));
                     var deser = JSON.Deserialize<ResponseOuter>(jsonString, DeserializationOptions);
                     return deser.ResponseInner;
